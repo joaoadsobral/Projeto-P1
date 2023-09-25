2495aa3c823f37b73b39e32dcf840a5de18b9c1d
João Antônio <120070923+joaoadsobral@users.noreply.github.com>
2023-09-25 19:03:56 -0300

    Add files via upload

--- Colisões/pedra.py
+++ Colisões/pedra.py
@@ -3,14 +3,15 @@ class Pedra(pygame.sprite.Sprite):
     def __init__(self, x, y):
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.image.load("Sprites/Colisões/Pedra.png") #Carrega a imagem da pedra que é o Obstaculo
-        self.image = pygame.transform.scale(self.image, (100, 120))  #Diminui o tamanho da imagem
+        self.image = pygame.transform.scale(self.image, (80, 80))  #Diminui o tamanho da imagem
         self.rect = self.image.get_rect()
         self.rect.center = (x, y)
         # Criar uma mascara para a pedra
         self.mask = pygame.mask.from_surface(self.image)
+
     def movimento(self):
         #toda vez que o obstaculo sair por completo da tela ele reaparece novamente
         if self.rect.topright[0] < 0:
-            self.rect.x = 1200
-        self.rect.x -= 20
+            self.rect.x = 2000
+        self.rect.x -= 10
 
